610d4268d6b4424d2981a91327ea70511eab990d
antonio <antoniobautista.desarrollo@gmail.com>
2026-03-20 00:53:18 +0000

    update methods to show  api get and post

--- src/main/java/com/storage/storageservice/controllers/ProductController.java
+++ src/main/java/com/storage/storageservice/controllers/ProductController.java
@@ -8,6 +8,8 @@ import com.storage.storageservice.models.ProductModel;
 import com.storage.storageservice.services.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +23,12 @@ public class ProductController {
     @Autowired
     ProductService productService;
     
+    @GetMapping()
     public List<ProductModel> getProducts(){
         return productService.listProducts();
     }
     
+    @PostMapping()
     public ProductModel setProduct(ProductModel product){
         return productService.saveProduct(product);
     }
